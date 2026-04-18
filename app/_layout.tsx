744edf08c47bf6a6f7c81d092c1958153f124e0d
Mayra Amaral <mayracunha18@gmail.com>
2026-04-18 00:14:41 -0300

    feat: adiciona tela inicial

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -47,6 +47,7 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="home" options={{ headerShown: false }} />
       </Stack>
       <StatusBar style="dark" />
     </ThemeProvider>
